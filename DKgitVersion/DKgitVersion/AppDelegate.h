568dfd0a481e6df076668f17900d5bf9639385b3
Heisenberg <Walte@Heisenberg.local>
2015-06-27 21:23:03 +0800

    首次提交

---
+++ DKgitVersion/DKgitVersion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKgitVersion
+//
+//  Created by Heisenberg on 15/6/27.
+//  Copyright (c) 2015年 Heisenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DKgitVersion/DKgitVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DKgitVersion
+//
+//  Created by Heisenberg on 15/6/27.
+//  Copyright (c) 2015年 Heisenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
